8f870fe255040c75e0ef354ff734f030b434a538
lily-ayers <lily.ayers@onshoreoutsourcing.com>
2019-09-13 14:46:50 -0500

    Modified Save Initialization in index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import WorldState from './world-state'
 
-let player;
+let worldState;
 
-if (localStorage.getItem('MSI-Player')) {
-    player = localStorage.getItem('MSI-Player');
+if (localStorage.getItem('MSI-WorldState')) {
+    worldState = localStorage.getItem('MSI-WorldState');
 } else {
-    player = newPlayer;
+    // Initialize New Game WorldState, Add Player's Held Amount of All Materials (0) to Every Materials Array
+    worldState = WorldState;
+    for (let era of WorldState) {
+        for (let mine of era.mines) {
+            for (let mat of mine.materials) {
+                mat.push("0");
+            }
+        }
+    }
 }
 
-ReactDOM.render(<App player={player} />, document.getElementById('root'));
+ReactDOM.render(<App worldState={worldState} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
