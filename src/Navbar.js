1c49e38a83f3f18e647d5489f5135fdd094aa771
pmbakerjohnson <xavier.richardson@LAPTOP-410>
2019-09-16 08:24:46 -0500

    Finished dropdown and navbar future changed needed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import Logo from './resources/icon-dropdown.png';
 
-const Navbar = (props) => {
+class Navbar extends Component {
+
+handleClick(){
+    let elem = document.getElementById("dropdown");
+    if(elem.className === "dropdown default"){
+        elem.className = "dropdown-open default";
+    }else{
+        elem.className = "dropdown default";
+    }
+}
+
+render(){
     return (
-        <div className="Navbar">
-            <button>Home</button>
-            <div className="dropdown">
-                <div className="listItem">
-                    <p className="header-small">Mine Name</p>
-                    <button>Mine!</button>
+        <div className="navbar">
+            <div className="dropdown default" id="dropdown" onClick={() => this.handleClick()}>
+                <img src={Logo} alt="logo" className="icon" />
+                <div className="listItem right default">
+                    <p className="header-small default" >Mine Name</p>
+                    <button >Mine!</button>
                 </div>
             </div>
         </div>
     )
+}
 };
 
 export default Navbar;
